491b9c78a09acd0ba764815d1c59b4ae362eca1d
Iury <iuryhorner@gmail.com>
2024-03-04 19:47:54 -0300

    start working on parsing client arguments

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request
+from flask import Flask, request, abort
+from app.app_utils import validate_filenames
 
 app = Flask(__name__)
 
@@ -6,12 +7,34 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+# The user can provide the argument "start" to get all data
+# Or a single timestamp which means data newer than that timestamp
+# Or two timestamps which means data in between those dates
+# I should put like DONE in a file to prevent this function from reading
+# from an unfinished file (writer still active)
+# I should also return the actual filename so that the client
+# can request newer files based on the returned one, otherwise a problem
+# might arise where they think they got all the data newer but there was
+# still files being written
+# I can make either a timestamp field and return in the JSON response
+# or I could've added timestamps to the records in the data, which is not
+# the approach I chose to make, so JSON field it is
+# Make this paginated?
+
+# make a filename duckdb db with timestamps only, once the client
+# requests range of files, i use duckdb to query which files should be opened
 @app.route('/data', methods=['GET'])
 def hand_out_json():
-    second = request.args.get("second")
-    minute = request.args.get("minute")
-    hour = request.args.get("hour")
-    d = {"hour": hour,
-         "minute": minute,
-         "second": second}
+    start = request.args.get("start")
+    end = request.args.get("end")
+
+    # validate start and end
+    if not validate_filenames(start, end):
+        abort(400, "Parameter passed with invalid ISO 8601 format")
+
+    d = {"This is what you provided as start": start,
+         "And as end": end}
     return d
+
+if __name__ == "__main__":
+    app.run(debug=True)
