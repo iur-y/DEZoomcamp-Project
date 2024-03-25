0c38d5d6befa420b89d3a54d435d45135ecdbe00
Iury <iuryhorner@gmail.com>
2024-03-25 15:20:42 -0300

    clean up comments

--- app/__main__.py
+++ app/__main__.py
@@ -9,22 +9,6 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
-# The user can provide the argument "start" to get all data
-# Or a single timestamp which means data newer than that timestamp
-# Or two timestamps which means data in between those dates
-# I should put like DONE in a file to prevent this function from reading
-# from an unfinished file (writer still active)
-# I should also return the actual filename so that the client
-# can request newer files based on the returned one, otherwise a problem
-# might arise where they think they got all the data newer but there was
-# still files being written
-# I can make either a timestamp field and return in the JSON response
-# or I could've added timestamps to the records in the data, which is not
-# the approach I chose to make, so JSON field it is
-# Make this paginated?
-
-# make a filename duckdb db with timestamps only, once the client
-# requests range of files, i use duckdb to query which files should be opened
 @app.route('/data', methods=['GET'])
 def hand_out_json():
     # validate request args
