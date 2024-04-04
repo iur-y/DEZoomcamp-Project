b14d7529da6223562f25afb20abcbee47e0a28f4
Iury <iuryhorner@gmail.com>
2024-04-04 15:37:33 -0300

    write to GCS instead of locally

--- producer/producer.py
+++ producer/producer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# producer.py: creates random videogame records and saves as parquet files
+# producer.py: creates random videogame records and saves as parquet files in GCS
 
 import pandas as pd
 import numpy as np
@@ -9,20 +9,10 @@ from random_utils import create_list_of_years, create_num_refunds,\
 from constants import BASEFILE, NUM_ITERATIONS, sale_columns, dtypes, schema
 import pyarrow as pa
 import pyarrow.parquet as pq
+from pyarrow.fs import GcsFileSystem
 from datetime import datetime, timezone
-import duckdb
 
-"""
-Next step: define my architecture, containers, airflow, BigQuery
-"""
-# TODO: writer = pq.ParquetWriter('./2024/02/28/24-02-28T18:42:30.parquet', schema) will not create directories, I have to create them, perhaps bash operator?
-# TODO: I'll have to check into limiting the resources used by a container so I can have this script and airflow running at the same time
-# and I might not want to run both the extraction script and the production script at the same time
-# remember that this one is just a script that runs a couple of times while app.py stays up 24/7
-# TODO: perhaps generate a new years list once I'm done writing a batch of files
-# TODO: parametrize the writer to build the path based on passed year/month/day and then use datetime.now() to generate filename
-# TODO: there's this optional ParquetWriter parameter write_statistics=True by default, maybe try to optimize the writer object
-
+BUCKET = "api_producer_data_zoomcamp_project" + "/"
 
 # Read file into DataFrame
 df = pd.read_csv(BASEFILE, dtype=dtypes, header=0)
@@ -30,20 +20,20 @@ df = pd.read_csv(BASEFILE, dtype=dtypes, header=0)
 # Create list of years from 1980 - 2020
 years = create_list_of_years()
 
-# get current time to name the file that is about to be created
+# Get current time to name the file that is about to be created
 dt_obj = datetime.now(timezone.utc)
 now = dt_obj.isoformat()
 
-# Create parquet writer, it does not create directories, only filename
-""" vvv READ THIS vvv """
-# By the way, when I create the directory 2024/03/04 with bash, I have to use UTC time to create them
-# and them pass those as parameters to this script so it can enter the correct directory
-# app.py will use UTC times to access the directories and read the files as well, so it's important
-year = dt_obj.year
-month = dt_obj.month
-day = dt_obj.day
+# Extract year, month and day from current time
+year = dt_obj.strftime('%Y')
+month = dt_obj.strftime('%m')
+day = dt_obj.strftime('%d')
 
-writer = pq.ParquetWriter(f'./data/{year}/{month}/{day}/{now}.parquet', schema)
+# Create writer object that writes directly to GCS bucket
+gcs = GcsFileSystem()
+writer = pq.ParquetWriter(f"{BUCKET}"
+                          f'{year}/{month}/{day}/{now}.parquet',
+                          schema=schema, filesystem=gcs)
 
 for _ in range(NUM_ITERATIONS): # around (1 MB, 16000 records) per iteration
 
@@ -63,13 +53,9 @@ for _ in range(NUM_ITERATIONS): # around (1 MB, 16000 records) per iteration
                     create_num_refunds(row.Rank, row.Global_Sales), axis=1)
     s.drop("Rank", axis=1, inplace=True)
 
+    # ~ 16k records per RecordBatch means 16k records in a parquet row group,
+    # which helps compression, but it's still a small number, as the recommended is 1 GB
     rb = pa.RecordBatch.from_pandas(s, preserve_index=False, schema=schema)
     writer.write_batch(rb)
 
 writer.close()
-
-# add filename entry to duckdb
-with duckdb.connect("./data/filenames.duckdb") as con:
-    con.sql("CREATE TABLE IF NOT EXISTS dates (dt_aware TIMESTAMPTZ)")
-    stmt = f"INSERT INTO dates VALUES ('{now}')"
-    con.sql(stmt)
