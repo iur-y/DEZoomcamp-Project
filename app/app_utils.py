1b481c6870c6fab18d88918757c7fd12c28c2b04
Iury <iuryhorner@gmail.com>
2024-04-04 15:15:07 -0300

    modify the app to read files from GCS instead of locally

--- app/app_utils.py
+++ app/app_utils.py
@@ -3,16 +3,17 @@
 # app_utils.py: utility functions to be used by app.py
 
 from dateutil import parser
-from datetime import timezone, datetime
+from datetime import datetime, timedelta
 import json # to convert dict to string in get_contents() function
-from typing import List, Tuple
 import pyarrow.parquet as pq
-
-import duckdb
+from pyarrow.fs import GcsFileSystem
+from google.cloud import storage
 
 valid_keys = frozenset(["start", "end"])
 empty_set = set()
 PAGESIZE = 200
+BUCKET = "api_producer_data_zoomcamp_project"
+gcs = GcsFileSystem()
 
 def validate_filenames(start, end) -> bool:
     try:
@@ -27,32 +28,73 @@ def validate_filenames(start, end) -> bool:
 def validate_args(d: dict) -> bool:
     return ((d.keys() - valid_keys) == empty_set) and ("start" in d)
 
+def add_1us(start):
+    """
+    Adds one microsecond to start
+    This facilitates calling the API with the start argument
+    """
+    one_us = timedelta(microseconds=1)
+    dt = datetime.fromisoformat(start)
+    new_dt = dt + one_us
+    return new_dt.isoformat()
 
-def get_filenames(*, start, end=None) -> List[Tuple[str]]:
+def prepend_yyyy_mm_dd(iso_dt):
+    # iso format: 
+    # 2024-03-26T19:58:21.575796+00:00
+    # drop everything that comes after T
+    tup = iso_dt.partition("T")
+
+    date = tup[0].split("-")
+    year = date[0]
+    month = date[1]
+    day = date[2]
+
+    return f"{year}/{month}/{day}/{iso_dt}.parquet"
+
+def iso_from_blob(blobname):
+    """
+    Extract valid iso 8601 datetime string from the full blob name
+    in GCS.
+
+    Example of blob name:
+        2024/03/26/2024-03-26T21:48:59.492914+00:00.parquet
+    Objective:
+        2024-03-26T21:48:59.492914+00:00
+    """
+    fname = blobname.removesuffix(".parquet")
+
+    # split on / and get the last element of the split
+    return fname.split("/")[-1]
+
+def list_blobs(bucket_name, start=None, end=None):
+
     if start == "beginning":
-        query = "SELECT * FROM dates"
-        if end:
-            query += f" WHERE dt_aware <= '{end}'"
+        start_offset = None
     else:
-        query = f"SELECT * FROM dates WHERE dt_aware > '{start}'"
-        if end:
-            query += f" AND dt_aware <= '{end}'"
-    with duckdb.connect("./data/filenames.duckdb") as con:
-        result = con.sql(query)
-        return result.fetchall()
+        start_offset = prepend_yyyy_mm_dd(add_1us(start))
+    
+    if end:
+        end_offset = prepend_yyyy_mm_dd(end)
+    else:
+        end_offset = None
+
+    storage_client = storage.Client()
+
+    # Note: Client.list_blobs requires at least package version 1.17.0.
+    blobs = storage_client.list_blobs(bucket_name,
+                                      start_offset=start_offset,
+                                      end_offset=end_offset)
+
+    # Note: The call returns a response only when the iterator is consumed.
+    for blob in blobs:
+        yield blob.name, iso_from_blob(blob.name)
 
 def get_contents(*, start, end=None):
-    tuples = get_filenames(start=start, end=end)
-    filenames = [t[0].astimezone(timezone.utc).isoformat() for t in tuples]
 
-    for fname in filenames:
-        dt = datetime.fromisoformat(fname)
-        year = dt.year
-        month = dt.month
-        day = dt.day
-        pq_file = pq.ParquetFile(
-            f"./data/{year}/{month}/{day}/{fname}.parquet")
+    for fname, iso_dt in list_blobs(BUCKET, start=start, end=end):
+
+        pq_file = pq.ParquetFile(f"{BUCKET}/{fname}", filesystem=gcs)
         for i in pq_file.iter_batches(PAGESIZE):
             d = i.to_pydict()
-            d["Timestamp"] = fname # add timestamp key to help client track
+            d["Timestamp"] = iso_dt # add timestamp key to help client track
             yield json.dumps(d) + "\n"
